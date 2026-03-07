8cfa82a5d6aadfda80c9aa195925c3c54da61f17
joaoVitor-amaro <joaovitoramaro71@gmail.com>
2026-03-07 16:56:27 -0300

    refactor(menu): move MenuPrincipal para pacote menu e reorganiza estrutura

--- src/main/java/com/smartstore/Main.java
+++ src/main/java/com/smartstore/Main.java
@@ -1,10 +1,9 @@
 package com.smartstore;
 
+import com.smartstore.menu.MenuPrincipal;
+
 public class Main {
-
     public static void main(String[] args) {
-
         MenuPrincipal.selecionarOptions();
-
     }
 }
--- src/main/java/com/smartstore/menu/MenuPrincipal.java
+++ src/main/java/com/smartstore/menu/MenuPrincipal.java
@@ -1,47 +1,38 @@
-package com.smartstore;
+package com.smartstore.menu;
 import java.util.Scanner;
 
 public class MenuPrincipal {
 
-    private static String[] options = {"Produtos", "Relatório", "Sair"}; // Lista de Opções do Menu
+    private static String[] options = {"Gerenciar Produtos", "Relatório", "Sair"}; // Lista de Opções do Menu
     private static Scanner sc = new Scanner(System.in);
 
     public static void exibirMenu() { // Exibe o menu e suas opções
-        System.out.println("======Bem-Vindo ao Menu Principal======");
+        System.out.println("======Bem-Vindo ao Smart Store======");
         for (int i = 0; i < options.length; i++) {
             System.out.println((i + 1) + ". " + options[i]);
         }
     }
 
     public static void selecionarOptions() {
-
         while (true) {
-
             exibirMenu();
             System.out.print("\nSelecionar opção: ");
-
             String escolha = sc.nextLine().toLowerCase();
-
             switch (escolha) {
-
                 case "produtos":
                 case "1":
                     System.out.println("Entrando no Menu Produtos...");
                     // MenuProdutos.iniciar();
                     break;
-
                 case "relatório":
                 case "relatorio":
                 case "2":
                     System.out.println("Entrando no Menu Relatório...");
-                    // MenuRelatorio.iniciar();
                     break;
-
                 case "sair":
                 case "3":
                     System.out.println("Saindo...");
                     return;
-
                 default:
                     System.out.println("Opção inválida. Tente novamente.\n");
             }
